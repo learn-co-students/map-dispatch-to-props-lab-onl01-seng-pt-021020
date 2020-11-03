6e3d4f0bef14773bfaa838fa5993b3873b38e7e7
jkatem <jenmiao@gmail.com>
2020-11-02 17:48:44 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 
+
+
 export class App extends Component {
   render() {
     return (
@@ -13,4 +15,6 @@ export class App extends Component {
   }
 };
 
-export default App;
+
+
+export default App
